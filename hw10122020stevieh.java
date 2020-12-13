9e922fd738a9ffd053afabda3965a229037341d5
Stevie H <74920914+stevie-h@users.noreply.github.com>
2020-12-13 21:59:11 +0200

    Create hw10122020stevieh.java

---
+++ hw10122020stevieh.java
@@ -0,0 +1,35 @@
+/* HW from 10.12.2020
+1. input two int numbers from the user a, b
+   print a > b, or a < b or a == b 
+   for example, if a = 3 and b = 4:
+       print "3 < 4"
+   for example, if a = 7 and b = 1:
+       print "7 > 1"
+   for example, if a = 9 and b = 9:
+       print "9 == 9"
+2. write a loop which prints all the numbers from 5 to 10
+3. write a loop which inputs a number from the user until a number bigger than 100 is given
+4. *etgar: print all the even numbers from 4 to 100.
+           should be like: 4, 6, 8, 10, ... 100
+5. *etgar: input two numbers from the user until the two numbers are the same
+
+*/
+
+
+package com.company;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+    
+    
+    
+    
+    
+    
+    
+    }
+}
